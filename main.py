a31ab7c49f05cde34a1ec200ef8990cbf4429c45
petko <p.todorov1015@gmail.com>
2023-07-29 22:57:35 +0300

    in progress

--- main.py
+++ main.py
@@ -20,17 +20,28 @@ class Game:
             while True:
                 try:
                     current_number = int(current_number)
-                    if 999 <= current_number <= 9999:
+                    if 999 <= current_number <= 9999 and len(set(str(current_number))) == 4:
                         break
                     else:
                         current_number = input("Enter a valid number (digits must be different): ")
                 except ValueError:
                     current_number = input("Enter a valid number (digits must be different): ")
 
+            self.moves += 1
+
             if current_number == random_number:
-                self.moves += 1
                 self.game_over = True
                 print(f"You won in {self.moves} moves!")
+                break
+
+            bulls, cows = 0, 0
+            for index, num in enumerate(str(current_number)):
+                if num == str(random_number)[index]:
+                    bulls += 1
+                if num in str(random_number) and num != str(random_number)[index]:
+                    cows += 1
+
+            print(f"Bulls: {bulls}, Cows: {cows}")
 
 
 game = Game()
